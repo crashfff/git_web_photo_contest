827cb1a55c38a900f280e10559e1db4d9f70ebe0
crashfff <neizvest1000@gmail.com>
2023-10-03 23:29:34 +0300

    add files

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class User(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+
+
+class Like(models.Model):
+    user = models.ForeignKey('User', on_delete=models.PROTECT, null=True)
+
+
+class Photo(models.Model):
+    description = models.CharField(max_length=255)
+    user = models.ForeignKey('User', on_delete=models.PROTECT, null=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey('User', on_delete=models.PROTECT, null=True)
