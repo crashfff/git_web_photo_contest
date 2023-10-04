dd6eed721cf863bec2cdb4b7af20dd9923cad81c
avshakhbanov <neizvest1000@gmail.com>
2023-10-04 11:54:44 +0300

    models update

--- app/models.py
+++ app/models.py
@@ -1,21 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class User(models.Model):
+class CustomUser(User):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
 
 
 class Like(models.Model):
-    user = models.ForeignKey('User', on_delete=models.PROTECT, null=True)
-    photo = models.ForeignKey('Photo', on_delete=models.PROTECT, null=True)
+    author = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
+    photo = models.ForeignKey('Photo', on_delete=models.CASCADE)
+    like_published = models.DateTimeField(null=True)
 
 
 class Photo(models.Model):
     description = models.CharField(max_length=255)
-    author = models.ForeignKey('User', on_delete=models.PROTECT, null=True)
+    author = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
+    photo_published = models.DateTimeField(null=True)
+    quantity_comments = models.IntegerField()
+    quantity_likes = models.IntegerField()
 
 
 class Comment(models.Model):
-    user = models.ForeignKey('User', on_delete=models.PROTECT, null=True)
-    photo = models.ForeignKey('Photo', on_delete=models.PROTECT, null=True)
+    author = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
+    photo = models.ForeignKey('Photo', on_delete=models.CASCADE)
+    comment_published = models.DateTimeField(null=True)
