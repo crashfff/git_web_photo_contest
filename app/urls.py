344afbf489fe696022194e18655d6ab0c8c72cfa
avshakhbanov <neizvest1000@gmail.com>
2023-10-11 11:49:59 +0300

    add few views

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('page_user/', views.page_user, name='page_user'),
+    path('photo/', views.photo, name='photo'),
 ]
+
--- app/views.py
+++ app/views.py
@@ -5,3 +5,13 @@ from django.template import loader
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
+
+
+def page_user(request):
+    template = loader.get_template('page_user.html')
+    return HttpResponse(template.render())
+
+
+def photo(request):
+    template = loader.get_template('photo.html')
+    return HttpResponse(template.render())
