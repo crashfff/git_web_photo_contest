91732896ca49d7162a170d6064d8dbe4238c5829
crashfff <neizvest1000@gmail.com>
2023-10-16 14:41:54 +0300

    not full change

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path('page_user/', views.page_user, name='page_user'),
     path('photo/', views.photo, name='photo'),
     path('all_users/', views.list_of_users, name='list_of_users'),
+    path('pub_photo/', views.pub_photo, name='pub_photo'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login, name='login'),
+    path('show_photo/<int:photo_id>', views.show_photo, name='show_photo')
 
 ]
 
--- app/views.py
+++ app/views.py
@@ -3,23 +3,50 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import *
 
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title':'Опубликовать фотографию','url_name': 'pub_photo'},
+        {'title': 'Служба поддержки', 'url_name': 'contact' },
+        {'title': 'Войти в профиль', 'url_name': 'login'}]
 
-menu = ['О сайте', 'Служба поддержки', 'Просто кнопка)']
 
 
 def index(request):
-    return render(request, 'index.html', {'menu':menu,'title': 'Главная страница'})
+        post = Photo.objects.all()
+        return render(request,
+         'index.html',
+             {'menu':menu, 'title': 'Главная страница', 'post': post})
 
 
 def page_user(request):
-    return render(request, 'page_user.html', {'title': 'Страница пользователя'})
+    context = {'menu': menu,
+               'title': 'Страница пользователя'
+               }
+    return render(request, 'page_user.html', context=context)
 
 
 def photo(request):
     post = Photo.objects.all()
-    return render(request, 'photo.html', {'list_of_photos': post, 'title': 'Фото'})
+    context = {'menu': menu,
+               'title': 'Страница пользователя'
+               }
+    return render(request, 'photo.html', {'menu': menu,'list_of_photos': post, 'title': 'Фото'})
 
 
 def list_of_users(request):
     post = CustomUser.objects.all()
-    return render(request, 'list_of_users.html', {'list_of_users': post, 'title': 'Главная страница'})
+    return render(request, 'list_of_users.html', {'menu': menu, 'list_of_users': post, 'title': 'Список всех пользователей'})
+
+def pub_photo(request):
+    return HttpResponse('Опубликование фотографии')
+
+def about(request):
+    return HttpResponse('Информация о сайте')
+
+def contact(request):
+    return HttpResponse('Служба поддержки')
+
+def login(request):
+    return HttpResponse('Авторизация')
+
+def show_photo(request, photo_id):
+    return HttpResponse(f'Фотография под id = {photo_id} ')
